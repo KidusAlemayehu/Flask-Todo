52908453cab29f40718edd81e8fbfb3329d68dd3
KidusAlemayehu <95192947+KidusAlemayehu@users.noreply.github.com>
2021-11-29 02:23:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://postgres:3346khag@localhost/Flask-Todo"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://postgres:'your password'@localhost/Flask-Todo"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
